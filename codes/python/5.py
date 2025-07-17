b1c9380347cb9e1a5c27fc26b5785daa31b6573a
Prakash Tajpuriya <86336006+prakashtaz0091@users.noreply.github.com>
2025-07-17 21:51:14 +0545

    Add files via upload

---
+++ codes/python/5.py
@@ -0,0 +1,103 @@
+# def give_num_5():
+#     return 5
+
+
+# result = 2 + give_num_5() + 5
+# print(result)
+
+
+# def test():
+#     result1 = 5
+#     result2 = 10
+
+#     return result1, result2  # (result1, result2) => packing => tuple => single data
+
+
+# # final_result = test()
+# # print(final_result)
+# r1, r2 = test()
+# print(r1)
+# print(r2)
+
+
+# def test():
+#     print("laksdfjl")
+
+
+# result = test()
+
+# print(result)
+
+
+# def factorial(num):
+#     if num == 1 or num == 0:
+#         return 1
+
+#     return num * factorial(num - 1)
+
+
+# fact_res = factorial(5)  # 5 * 4 * 3 * 2 * 1
+
+# print(fact_res)
+
+
+"""
+def entry_point():
+    res = check_if_user_is_authenticated()
+    if res:
+        send_mail()
+    else:
+        return not_auth_page()
+
+"""
+
+"""
+1. Built-in modules
+2. User-defined modules
+3. Third-party modules
+"""
+# import math
+
+
+# print(math.pi)
+
+# radius = float(input("Enter the radius of circle: "))
+
+# print(f"Area of circle of radius {radius} is {math.pi * radius**2}")
+
+# print(math.sqrt(4))
+# print(math.cbrt(8))
+# print(math.isnan("123"))
+
+# GRAVITY = 9.8
+
+
+# import helpers  # here, helpers.py is a module
+
+# # helpers.greet_user("Ram")
+# # helpers.add_all(2, 3, 4)
+# print(helpers.PI)
+
+# from helpers import greet_user, PI
+# from helpers import greet_user as
+# import helpers as hp
+
+# hp.add_all()
+# hp.greet_user
+
+
+# greet_user("ram")
+# print(PI)
+# gt("ram")
+
+# import helpers
+
+# print("Print from 5.py, ", __name__)
+# a = 5
+
+
+# print(globals)
+
+# from custom_pack import helpers as hp
+
+# hp.greet_user("ram")
---
+++ codes/python/custom_pack/helpers.py
@@ -0,0 +1,19 @@
+def greet_user(name):
+    print(f"Welcome here, {name}")
+
+
+def add_all(*numbers):
+    result = sum(numbers)
+    print(result)
+
+
+PI = 22 / 7
+
+# print("Print from helpers.py ", __name__)
+
+# if __name__ == "__main__":
+#     print(__name__)
+#     print("This module is running as main script")
+
+#     print("Testing greet user")
+#     greet_user("ram")
